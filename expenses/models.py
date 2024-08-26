6e08c432171811286fd8a8ae14a5cd6eeb55f4e3
robert89th <robertdirr@icloud.com>
2024-08-26 16:35:54 -0500

    models fix

--- expenses/models.py
+++ expenses/models.py
@@ -10,3 +10,13 @@ class Expense(models.Model):
 
     def __str__(self):
         return f'{self.category}: {self.amount}'
+    from django.db import models
+
+class Expense(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.category}: {self.amount}'
