fa08157da09cc1503156ffb5d149213e6f2976b4
robert89th <robertdirr@icloud.com>
2024-08-26 15:21:35 -0500

    urls.py fix

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('expenses.urls')),  # Include the URLs from the expenses app
+    path('', include('expenses.urls')),  # Routing to the expenses app
 ]
