166beaf5702d802dd13c9714bf30739e5966a7d0
robert89th <robertdirr@icloud.com>
2024-08-26 13:56:05 -0500

    urls.py fix

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path
-from ..expenses import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.expense_list, name='expense_list'),
-    # Add paths for adding, editing, and deleting expenses
-]
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+    path('', include('expenses.urls')),  # Include the URLs from the expenses app
 ]
